8d9119ff1f7fd5fb0fdd822163562ecdb88ec5ac
liaoyuezhen <1315472685@qq.com>
2019-12-21 11:14:08 +0800

    完成，按需加载不行

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 /**
  * ES6 的 class 来定义组件与 JavaScript 函数组件在 React 里是等效的。
  */
-/**
- * 
- class App extends React.Component {
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" /></header>
-        
+        <header className="App-header">
+          <Button type="primary">Primary</Button>
+          <Button type="primary" shape="circle" icon="search" />
+          <img src={logo} className="App-logo" alt="logo" /></header>
+
       </div>
 
     )
   }
 }
- * 
- */
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import 'antd/dist/antd.css';
+import 'antd/es/date-picker/style/css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
