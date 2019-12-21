5fa2b5f555b13e83f9eca6d24eaa44046c9a4127
liaoyuezhen <1315472685@qq.com>
2019-12-21 14:45:18 +0800

    PC端按需加载以及scss完成

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import { DatePicker } from 'antd';
-import 'antd/dist/antd.css';
-// import 'antd/es/date-picker/style/css';
+import { Button } from 'antd';
 import './App.css';
 import './App.scss';
-// import { postFn } from './view/axios/http'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-/**
- * ES6 的 class 来定义组件与 JavaScript 函数组件在 React 里是等效的。
- */
 
 class App extends React.Component {
   componentDidMount() {
@@ -47,7 +20,8 @@ class App extends React.Component {
         <header className="App-header">
 
           <div className="box"></div>
-          <DatePicker />
+          <br />
+          <Button type="primary">Primary</Button>
           <h2>Hello World</h2>
           <img src={logo} className="App-logo" alt="logo" /></header>
 
